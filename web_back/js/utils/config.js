7c649a5e17de81227b1d44dbbe52ad4979efcf42
**** <1069319584@qq.com>
2019-09-23 16:38:48 +0800

    文章类别管理功能，添加删除功能完成2.0

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -2,5 +2,11 @@ var BASE_URL = 'http://localhost:8000';
 var URLIST = {
   user_login: BASE_URL + '/admin/login',
   user_logout: BASE_URL + '/admin/logout',
-  user_info: BASE_URL + '/admin/getuser'
+  user_info: BASE_URL + '/admin/getuser',
+  // 文章类别
+  // 显示类别
+  category_show: BASE_URL + '/admin/category_search',
+  category_add: BASE_URL + '/admin/category_add',
+  category_delete: BASE_URL + '/admin/category_delete'
+ 
 };
